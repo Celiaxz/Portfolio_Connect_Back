ce23a70ec48e202cd37e65dd1ba9f44dbc77cc12
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-08-07 20:27:43 +0200

    add github field

--- models/User.model.js
+++ models/User.model.js
@@ -2,37 +2,46 @@ const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
+    githubUsername: {
+      type: String,
+      required: [true, "Username is required."],
+      trim: true,
+    },
     username: {
       type: String,
-      required: [true, 'Username is required.'],
+      required: [true, "Username is required."],
       unique: true,
-      trim: true
+      trim: true,
     },
     email: {
       type: String,
-      required: [true, 'Email is required.'],
+      required: [true, "Email is required."],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
     password: {
       type: String,
-      required: [true, 'Password is required.']
+      required: [true, "Password is required."],
     },
-    projects: [{
-      type: Schema.Types.ObjectId,
-      ref: "Project"
-    }],
-    skills: [{
-      type: String,
-      trim: true,
-    }],
+    projects: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+      },
+    ],
+    skills: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
     image: String,
-    aboutMe: String
+    aboutMe: String,
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
   }
 );
 
